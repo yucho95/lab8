a0d7474688f0f53514de250d493f0dbd9cebcd7a
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-03-01 23:35:11 -0800

    Added about page template code

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars');
 
 var index = require('./routes/index');
+var about = require('./routes/about');
 
 // Example route
 // var user = require('./routes/user');
@@ -37,6 +38,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/about', about.view);
 
 // Example route
 // app.get('/users', user.list);
---
+++ routes/about.js
@@ -0,0 +1,7 @@
+/*
+ * GET about page.
+ */
+exports.view = function(req, res){
+  res.render('about', { });
+};
+
